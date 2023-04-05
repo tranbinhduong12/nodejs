85bc63e36c47f108f4c542a71bb47fef7f6e254b
dwg1207 <tranduong12072001@gmail.com>
2023-04-05 22:07:01 +0700

    backend final

--- backend/controllers/HomeController.js
+++ backend/controllers/HomeController.js
@@ -8,6 +8,28 @@ const HomeController = {
     index: async(req, res) => {
         // render
         res.render('pages/index');
+    },
+    category: async(req, res) => {
+        // render
+        // get id from url
+        const id = req.params.id;
+        res.render('pages/categories');
+    },
+    contact: async(req, res) => {
+        // render
+        res.render('pages/contact');
+    },
+    product: async(req, res) => {
+        // render
+        res.render('pages/product');
+    },
+    cart: async(req, res) => {
+        // render
+        res.render('pages/cart');
+    },
+    checkout: async(req, res) => {
+        // render
+        res.render('pages/checkout');
     }
     
 }
--- backend/routes/Home.js
+++ backend/routes/Home.js
@@ -3,5 +3,10 @@ const OrderController = require('../controllers/HomeController');
 const router = require('express').Router();
 
 router.get('/', OrderController.index);
+router.get('/category/:id', OrderController.category);
+router.get('/contact', OrderController.contact);
+router.get('/product', OrderController.product);
+router.get('/cart', OrderController.cart);
+router.get('/checkout', OrderController.checkout);
 
 module.exports = router;
